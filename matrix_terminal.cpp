0c263c6f76bc4e72c834819dee4c17075c381322
Emanuel Sanchez <111335411+ESanchez3303@users.noreply.github.com>
2025-03-12 18:28:35 -0500

    Update matrix_terminal.cpp
    adding the pause at the end of the program to view results

--- matrix_terminal.cpp
+++ matrix_terminal.cpp
@@ -1,6 +1,6 @@
 //Team#: 8
-//Team Member - 1 : Emanuel Sanchez Member’s Contribution(in%) 50
-//Team Member - 2 : Dion Bobino Member’s Contribution(in%) 50
+//Team Member - 1 : Emanuel Sanchez Memberâ€™s Contribution(in%) 50
+//Team Member - 2 : Dion Bobino Memberâ€™s Contribution(in%) 50
 
 
 #include "matrixType.h";  // Including the H File to make the objects
@@ -22,6 +22,10 @@ int main() {
 	cout << (m1 + m2);                                             // Overload + operator
 	cout << (m1 - m2);											   // Overload - operator
 	cout << (m1 * m2);											   // Overload * operator
+
+	string tempString;
+	cout << "Push enter to continue..." << endl;
+	cin >> tempString;
 	
 	return 0;
 }
